0d173327a00a0f64b0b886f727eed0910186de08
VictoriqueMoe <loli@victorique.moe>
2025-12-29 21:14:54 +0000

    fix build (final maybe)

--- app/list/[uuid]/opengraph-image.tsx
+++ app/list/[uuid]/opengraph-image.tsx
@@ -86,7 +86,6 @@ export default async function OGImage({ params }: { params: Promise<{ uuid: stri
                             overflow: "hidden",
                         }}
                     >
-                        {/* eslint-disable-next-line */}
                         <img
                             src={cover}
                             style={{
