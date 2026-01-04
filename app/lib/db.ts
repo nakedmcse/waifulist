4f43220186ce545640223e915ec475797435f7ef
VictoriqueMoe <loli@victorique.moe>
2026-01-04 20:06:23 +0000

    modify journal_mode to WAL

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -27,6 +27,7 @@ if (!fs.existsSync(dataDir)) {
 const dbPath = path.join(dataDir, "waifulist.db");
 const db = new Database(dbPath);
 
+db.pragma("journal_mode = WAL");
 db.pragma("foreign_keys = ON");
 
 db.exec(`
